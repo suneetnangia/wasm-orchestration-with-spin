2cf0f02f01746aa84ec9993e0d39773b06c8c6ba
Marcel Bindseil <marcelbindseil@gmail.com>
2023-06-16 17:02:33 +0200

    Feature/dev setup (#1)
    * validated dev setup on macos, linux
    
    * reviewed readme, fixed dev container extensions
    
    * handled PR review comments (removed commented out dev container config, variable in shell script)

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,8 +1,10 @@
 # Installs latest stable toolchain for Rust and clippy/fmt for this toolchain
-rustup update stable && rustup default stable && rustup component add clippy rustfmt
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+PATHRUSTUP=$HOME/.cargo/bin
+$PATHRUSTUP/rustup update stable && $PATHRUSTUP/rustup default stable && $PATHRUSTUP/rustup component add clippy rustfmt
 
 # Installs wasm32 compiler targets
-rustup target add wasm32-wasi wasm32-unknown-unknown
+$PATHRUSTUP/rustup target add wasm32-wasi wasm32-unknown-unknown
 
 # Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
