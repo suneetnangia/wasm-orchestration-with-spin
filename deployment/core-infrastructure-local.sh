6da9b4e6b30fac3b0424b2174db30fd2015e873f
Marcel Bindseil <marcelbindseil@gmail.com>
2023-07-06 15:54:30 +0200

    port forwarding, redis helm chart from public repo

--- deployment/core-infrastructure-local.sh
+++ deployment/core-infrastructure-local.sh
@@ -1,24 +1,27 @@
-shimSpinDir=../wasm-shims/containerd-shim-spin-v1
 dockerDir=../wasm-shims/deployments/k3d
-tmpSpinDir=../wasm-shims/deployments/k3d/.tmp/containerd-shim-spin-v1
+tmpSpinDir=../wasm-shims/deployments/k3d/.tmp
 
-
-
-# Build Spin shim
-# cargo build --release --manifest-path=$shimSpinDir/Cargo.toml
-cp -p ../apps/runtime/containerd-shim-spin-v1 $tmpSpinDir
+# Copy Spin shim
+mkdir -p $tmpSpinDir
+cp -p ../apps/runtime/containerd-shim-spin-v1 $tmpSpinDir/containerd-shim-spin-v1
 
 # Build k3d image
 docker build -t k3d-shim -f $dockerDir/Dockerfile $dockerDir
 
 # Create k3d cluster
-k3d cluster create wasm-cluster --image k3d-shim -p '8081:80@loadbalancer' --servers 1
+k3d cluster create wasm-cluster --image k3d-shim --api-port 6551 -p '8001:30010@loadbalancer' --servers 1
 
 # Load Spin runtime
 kubectl apply -f ../apps/runtime/runtime.yaml
 
 # Deploy Redis (pull public Redis image, save it to local directory and import into k3d cluster)
-helm install redis-stack ./helm/redis-stack --set-string redis_stack.tag="latest" --namespace redis --create-namespace --wait
+helm repo add redis-stack https://redis-stack.github.io/helm-redis-stack/
+helm repo update
+helm upgrade --install redis-stack redis-stack/redis-stack --set-string redis_stack.tag="latest" --reuse-values --namespace redis --create-namespace --wait
+# Replace Helm service
+kubectl apply -f ./helm/redis-stack/service.yaml -n redis
+
+# Build and load workloads
 buildWorkloads()
 {
   cd ../apps/$1
@@ -26,8 +29,8 @@ buildWorkloads()
   cp -r ../../target .
   # spin k8s build
   mkdir -p tmp
-  docker buildx build -f Dockerfile -t $1:latest . --load --platform=wasi/wasm32 --provenance=false
-  docker save -o tmp/$1.tar $1:latest
+  docker buildx build -f Dockerfile -t $1:v1 . --load --platform=wasi/wasm32 --provenance=false
+  docker save -o tmp/$1.tar $1:v1
   k3d image import tmp/$1.tar -c wasm-cluster
   rm -r tmp
   spin k8s deploy
