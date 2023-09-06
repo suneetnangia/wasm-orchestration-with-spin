ce140b5a0fcce9187e0499290a86c7e1ad1cb855
Marcel Bindseil <marcelbindseil@gmail.com>
2023-09-06 09:52:29 +0200

    Feature/cicd (#16)
    * updated description
    
    * added labeling for image and push to make/script
    
    * renamed script

---
+++ deployment/build-push-workload-image.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+IMAGENAME=ghcr.io/$3/$1
+LABEL=org.opencontainers.image.source=https://github.com/$3/$4
+
+cd $2/$1
+
+# build the app
+spin build
+cp -r ../../target .
+
+# get the version from the spin.toml file
+VERSION=`grep ^version spin.toml | cut -d'"' -f 2`
+
+# build the docker image
+docker buildx build -f Dockerfile -t $IMAGENAME:$VERSION -l $LABEL --load --platform=wasi/wasm32 --provenance=false .
+
+# tag the docker image
+docker tag $IMAGENAME:$VERSION $IMAGENAME:latest
+
+docker push $IMAGENAME:$VERSION
+docker push $IMAGENAME:$latest
+
+cd ../..
