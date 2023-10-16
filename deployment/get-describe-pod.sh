6083c502937ae00483530b1ec0a7e1c590702804
Marcel Bindseil <marcelbindseil@gmail.com>
2023-10-16 18:44:49 +0200

    verfication step

---
+++ deployment/get-describe-pod.sh
@@ -0,0 +1,8 @@
+# get the version from the spin.toml file
+POD=`kubectl get pods | awk '/orderpro/ {print $1;exit}'`
+echo "====================DESCRIBE===================="
+kubectl describe pods/$POD
+echo "======================LOGS======================"
+kubectl logs $POD
+echo "====================DISK SIZE==================="
+df -H
