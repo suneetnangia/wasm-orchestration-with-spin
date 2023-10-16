ad5c34e31cfbd350321ecc68d1c75c84f5ac5ee2
Marcel Bindseil <marcelbindseil@gmail.com>
2023-10-16 13:43:39 +0000

    fixed deleted command

--- deployment/get-describe-pod.sh
+++ deployment/get-describe-pod.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # get the version from the spin.toml file
+POD=`kubectl get pods | awk '/orderpro/ {print $1;exit}'`
 echo "====================DESCRIBE===================="
 kubectl describe pods/$POD
 echo "======================LOGS======================"
